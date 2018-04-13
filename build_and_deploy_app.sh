02323f504e889a9a4335d89406cfb9267feebd31
Ampie Barnard <ampie.private@gmail.com>
2018-04-13 10:05:45 +0200

    Got first version working with both S2I images

---
+++ build_and_deploy_app.sh
@@ -0,0 +1,9 @@
+
+oc login -u system:admin
+oc project myproject
+oc delete deploymentconfig entando-sample
+oc delete service entando-sample-service
+oc delete route entando-sample-service
+oc login -u developer -p developer
+rm overlays -rf
+mvn clean process-resources fabric8:build fabric8:resource fabric8:apply -Popenshift -DskipTests
--- build_and_deploy_db.sh
+++ build_and_deploy_db.sh
@@ -1,3 +1,4 @@
+
 oc login -u system:admin
 oc project myproject
 oc delete deploymentconfig entando-sample-db
@@ -6,4 +7,4 @@ oc delete route entando-sample-db-service
 oc delete pvc entando-sample-db-pvc
 oc login -u developer -p developer
 rm overlays -rf
-mvn clean process-resources fabric8:build fabric8:resource fabric8:apply -DskipTests
+mvn clean process-resources fabric8:build fabric8:resource fabric8:apply -Ppostgresql -DskipTests
